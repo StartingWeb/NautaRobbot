7690772fd20eb08b8f6e5593a58d9cbc36f9d2b0
Starting Web <startingwebtec@gmail.com>
2025-06-18 11:31:28 -0300

    push inicial

---
+++ Facilitadores.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NautaRobbot
+{
+    public static class Fac
+    {
+        public static bool ValidarSenhaHash(string senha, string hashDataBase)
+        {
+            string hashSenhaAtual = BCrypt.Net.BCrypt.HashPassword(senha);
+            return BCrypt.Net.BCrypt.Verify(senha, hashDataBase);
+        }
+
+        public static string RetornaSenhaHash(string senha)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(senha);
+        }
+    }
+}
